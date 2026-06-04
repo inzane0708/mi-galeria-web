77d542c4377715e763f845725211a8bc8106e88b
Laboratorios Santiago Oriente <sor_laboratorio@inacap.cl>
2026-06-04 19:54:35 -0400

    agrega buscador pokemon

--- js/app.js
+++ js/app.js
@@ -1,49 +1,72 @@
 const galeria = document.getElementById("galeria");
+const buscador = document.getElementById("buscar");
 
-async function cargarDatos(){
+let listaPokemon = [];
+
+async function cargarDatos() {
 
     galeria.innerHTML = "Cargando...";
 
-    try{
+    try {
 
         const respuesta =
         await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
 
-        if(!respuesta.ok){
+        if (!respuesta.ok) {
             throw new Error("Error " + respuesta.status);
         }
 
         const datos = await respuesta.json();
 
-        galeria.innerHTML = "";
+        listaPokemon = [];
 
-        for(const pokemon of datos.results){
+        for (const pokemon of datos.results) {
 
             const detalle = await fetch(pokemon.url);
             const info = await detalle.json();
 
-            const tarjeta =
-            document.createElement("article");
-
-            tarjeta.className = "tarjeta";
-
-            tarjeta.innerHTML = `
-                <img src="${info.sprites.front_default}" alt="${info.name}">
-                <h3>${info.name}</h3>
-            `;
-
-            galeria.appendChild(tarjeta);
+            listaPokemon.push(info);
         }
 
-    }catch(error){
+        mostrarPokemon(listaPokemon);
 
-        galeria.innerHTML =
-        "No se pudieron cargar los datos.";
+    } catch (error) {
 
+        galeria.innerHTML = "No se pudieron cargar los datos.";
         console.error(error);
     }
 }
 
+function mostrarPokemon(lista) {
+
+    galeria.innerHTML = "";
+
+    lista.forEach(info => {
+
+        const tarjeta = document.createElement("article");
+
+        tarjeta.className = "tarjeta";
+
+        tarjeta.innerHTML = `
+            <img src="${info.sprites.front_default}" alt="${info.name}">
+            <h3>${info.name}</h3>
+        `;
+
+        galeria.appendChild(tarjeta);
+    });
+}
+
+buscador.addEventListener("input", () => {
+
+    const texto = buscador.value.toLowerCase();
+
+    const filtrados = listaPokemon.filter(pokemon =>
+        pokemon.name.includes(texto)
+    );
+
+    mostrarPokemon(filtrados);
+});
+
 document
-.getElementById("cargar")
-.addEventListener("click", cargarDatos);
+    .getElementById("cargar")
+    .addEventListener("click", cargarDatos);
